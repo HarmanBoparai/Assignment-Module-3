b887e25152d86b08ebb53571a5b0f44ba5208a2f
HarmanBoparai <erharmanboparai@gmail.com>
2022-09-15 17:24:14 -0400

    generated functions

--- script.js
+++ script.js
@@ -1,10 +1,97 @@
 // Assignment code here
+var lowercase=[  'a',
+'b',
+'c',
+'d',
+'e',
+'f',
+'g',
+'h',
+'i',
+'j',
+'k',
+'l',
+'m',
+'n',
+'o',
+'p',
+'q',
+'r',
+'s',
+'t',
+'u',
+'v',
+'w',
+'x',
+'y',
+'z',
+];
+var uppercase=['A',
+'B',
+'C',
+'D',
+'E',
+'F',
+'G',
+'H',
+'I',
+'J',
+'K',
+'L',
+'M',
+'N',
+'O',
+'P',
+'Q',
+'R',
+'S',
+'T',
+'U',
+'V',
+'W',
+'X',
+'Y',
+'Z',
+];
 
+var numbers=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialcharacters=['@',
+'%',
+'+',
+'\\',
+'/',
+"'",
+'!',
+'#',
+'$',
+'^',
+'?',
+':',
+',',
+')',
+'(',
+'}',
+'{',
+']',
+'[',
+'~',
+'-',
+'_',
+'.',
+];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function passwordOptions()
+{
+var pwLength =parseInt(prompt("Must be between 8 and 128 characters"));
+var pwLowercase=confirm("Would you like to include lowercase");
+}
 // Write password to the #password input
+function generatePassword(){
+
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -12,6 +99,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

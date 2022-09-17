4a3718fa00aaa21052fb7073bf77a70714539960
HarmanBoparai <erharmanboparai@gmail.com>
2022-09-16 23:09:49 -0400

    added comments to the js file

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Assignment code here
+
 var generateBtn=document.querySelector("#generate")
-var lowercase=[  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+// Created Arrays  for Lowercase,Numbers,Uppercase,Special Chars And An Empty Array to store the values
+var lowercase=[  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];  
 var uppercase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 var numbers=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialcharacters=['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
 var pwdoptions=[];
 
+//the following function returns a random integer between the specified values.
 function randomInt(min,max){
   if(!max) {
 max=min
@@ -15,10 +17,12 @@ min=0
   return Math.floor(min*(1-rand)+rand*max)
 }
 
+//This  function to used get a random item from an array.
 function getRandomItem(list){
 return list[randomInt(list.length)]
 }
 
+//This function generates the password provided the given conditions or criteria is met
 function generatePassword(){
   while (true){
 
@@ -35,11 +39,13 @@ function generatePassword(){
 }
   }
 
+  //The following creates the different types of the user prompts
 var userWantNumbersinpwd = window.confirm("Do you like to include numbers in your password?")
 var userWantSymbolsinpwd = window.confirm("Do you like to include symbols in your password?")
 var userWantLowercaseinpwd = window.confirm("Do you like to include lowercase letters in your password?")
 var userWantSymbolsinpwd = window.confirm("Do you like to include uppercase in your password?")
 
+//Criteria and conditions to be validated
 if (userWantNumbersinpwd===true){
 pwdoptions.push(numbers)
 }
@@ -59,13 +65,14 @@ if (pwdoptions.length===0){
         pwdoptions.push(uppercase)
         }
 
+// The following will contain the random words 
 var generatePassword= ""
 for(var i=0;i<pwdLength;i++){
   var randomlist=getRandomItem(pwdoptions)
   var randomchar=getRandomItem(randomlist)
   generatePassword += randomchar
 }
-return generatePassword
+return generatePassword  //It will return the generated password
     }
 
 // Get references to the #generate element
